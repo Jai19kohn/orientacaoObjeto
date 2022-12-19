a3c519d8d4e784074376cb113e020c4909ca2a5b
Jaine <jaine2005kohn@gmail.com>
2022-12-19 14:19:47 -0300

    atualização

---
+++ src/mediaIdades/idade.java
@@ -0,0 +1,8 @@
+package mediaIdades;
+
+public class idade {
+
+
+}
+// Escreva um programa OO que lê nome e idade de 5 pessoas e
+// ao final informa quem é o mais novo, o mais velho e qual a média de idade.
